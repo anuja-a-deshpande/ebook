3680b4cff4724978f42988acaf619d073e458d29
Anuja Deshpande <anuja.deshpande@spherity.com>
2022-06-08 17:58:43 +0200

    Navigation fixed

--- App.jsx
+++ App.jsx
@@ -1,31 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { StyleSheet, Text, View ,AppRegistry, Platform} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './src/Navigation/RootNavigator'
+import { registerRootComponent } from 'expo';
+import * as React from 'react';
+import { AppRegistry, Platform } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { name as appName } from './app.json';
+import RootNavigator from './src/Navigation/RootNavigator';
 
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
 if (Platform.OS === 'android') {
   registerRootComponent(App);
 } else {
   AppRegistry.registerComponent(appName, () => App);
 }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </View>
-  );
-}
+const App = () => (
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  </SafeAreaProvider>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
