fa1faaecb079ba087dc219be69ea5596ffafb818
Anuja Deshpande <anuja.deshpande@spherity.com>
2022-06-15 14:02:51 +0200

    Home Page UI

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,10 +1,158 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View, StyleSheet, Image, Text, ScrollView, Dimensions } from "react-native";
+import Swiper from 'react-native-swiper';
+import { Entypo, FontAwesome, FontAwesome5 } from '@expo/vector-icons';
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Home () {
-    return (
+const Home = () => {return (
     <View>
-    <Text>HomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHomeHome</Text>
+
+      <LinearGradient
+        colors={['skyblue', 'blue']}
+        style={{ height: "10%", marginBottom: 10}}
+      >
+      </LinearGradient>
+
+      <ScrollView style={styles.container}>
+
+        <View>
+          <Swiper
+            autoplay
+            horizontal={true}
+            height={200}
+            activeDotColor='skyblue'>
+              <Image
+                source={{
+                  uri: 'https://www.scholastic.com/content/dam/scholastic/educators/book-lists/Harry-Potter-Book-List_BL_16-9.jpg.corpimagerendition.xxl.1400.788.png',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&fit=clip',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://cdn.coolofthewild.com/wp-content/uploads/2016/12/adventure-books.jpg',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/02/27/11/pile-of-books.jpg',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1551029506-0807df4e2031?ixlib=rb-1.2.1&auto=format&fit=crop&w=891&q=80',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1549122728-f519709caa9c?ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+              <Image
+                source={{
+                  uri: 'https://blog.efpsa.org/wp-content/uploads/2016/06/programming-psychologist-python-elixir-rstats-640x497.jpg',
+                }}
+                resizeMode="cover"
+                style={styles.sliderImage}
+              />
+          </Swiper>
+
+        </View>
+
+        <View style={styles.categoryContainer}>
+
+          <TouchableOpacity style={styles.categoryBtn}>
+            <View style={styles.categoryIcon}>
+            {/* <LinearGradient
+        colors={['skyblue', 'blue']}
+        // style={{ height: "10%", marginBottom: 10}}
+      > */}
+      
+              <FontAwesome name="bookmark" size={35} color='skyblue' />
+              {/* </LinearGradient> */}
+            </View>
+            <Text style={styles.categoryBtnTxt}>Saved</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.categoryBtn}>
+            <View style={styles.categoryIcon}>
+              <FontAwesome5 name="readme" size={35} color='skyblue' />
+            </View>
+            <Text style={styles.categoryBtnTxt}>History</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.categoryBtn}>
+            <View style={styles.categoryIcon}>
+              <Entypo name="star" size={35} color='skyblue' />
+            </View>
+            <Text style={styles.categoryBtnTxt}>Favourite</Text>
+          </TouchableOpacity>
+
+        </View>
+      </ScrollView>
     </View>
-    )
-}
+    
+  );
+
+};
+
+const styles = StyleSheet.create({
+  sliderImage: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center'
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  categoryBtn: {
+    flex: 1,
+    marginHorizontal: 0,
+    alignSelf: 'center',
+    marginLeft: 15
+  },
+  categoryIcon: {
+    borderWidth: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 70,
+    height: 70,
+    borderRadius: 50,
+    marginRight: 30
+  },
+  categoryBtnTxt: {
+    alignSelf: 'center',
+    marginTop: 5,
+    color: 'skyblue',
+    marginRight: 25
+  }
+});
+export default Home;
